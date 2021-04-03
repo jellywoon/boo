89253c6988e4d21983bfbd7847aa0d58c4abe0c3
jellywoon <81005979+jellywoon@users.noreply.github.com>
2021-04-03 10:40:41 +0300

    Add files via upload

---
+++ string.py
@@ -0,0 +1,13 @@
+n = int(input())
+strings = []
+for i in range(n):
+    string = input()
+    if string[0] == '%' and string[1] == '%':
+        string = string.replace('%%', '')
+        strings.append(string)
+    elif string[0] != '#' and string[1] != '#' and string[2] != '#' and string[3] != '#':
+        strings.append(string)
+for k in strings:
+    print(k)
+
+
